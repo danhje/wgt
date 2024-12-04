fc282a95bb94a881a995ca878a35e776850b67a3
Daniel Hjertholm <daniel.hjertholm@statnett.no>
2024-12-04 12:25:30 +0100

    Replace poetry with uv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import os
 
 
-def pytest_configure():
+def pytest_configure() -> None:
     os.environ["WGT_DEFAULT_PROTOCOL"] = "https://"
     os.environ["WGT_DEFAULT_HOST"] = "127.0.0.1"
     os.environ["WGT_DEFAULT_PORT"] = ":8080"
